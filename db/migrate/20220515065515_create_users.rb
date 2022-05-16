1db5c5d63a5353414f6c085ab2379188c2a0ce69
Billy <billy.phan4@gmail.com>
2022-05-16 15:15:14 -0700

    migration table redo

---
+++ db/migrate/20220515065515_create_users.rb
@@ -0,0 +1,14 @@
+class createUsers < ActiveRecord::Migration[5.2]
+    def change
+        create_table :users do |t|
+            t.string "name", null: false
+            t.string "email", null: false
+            t.string "password_digest", null: false
+            t.string "session_token", null: false
+            t.timestamps
+        end
+    end
+  end
+  
+
+
---
+++ db/migrate/20220516215706_redo_table.rb
@@ -0,0 +1,5 @@
+class RedoTable < ActiveRecord::Migration[5.2]
+  def change
+    add_index :users, :created_at
+  end
+end
