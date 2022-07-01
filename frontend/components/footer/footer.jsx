d23e221a2cd5f2c1d83dc5f130b2c1e8959fa15e
Billy <billy.phan4@gmail.com>
2022-07-01 15:26:54 -0700

    target blank add

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
             <ul className='footer-container'>
                 <li><Link to='/'><img className='whiteLogo' src={window.white_logo} alt="" /></Link></li>
                 <li><a href="https://www.linkedin.com/in/billy-phan-41014a40/" target="_blank"><FaLinkedin color='white'size={50} /></a></li>
-                <li><a href='https://github.com/bphan002'><FaGithubSquare color='white' size={50} /></a></li>
-                <li><a href='https://bphan002-porftolio-website.herokuapp.com/'><GiHighKick color='white' size={50} /></a></li>
+                <li><a href='https://github.com/bphan002' target="_blank"><FaGithubSquare color='white' size={50} /></a></li>
+                <li><a href='https://bphan002-porftolio-website.herokuapp.com/' target="_blank"><GiHighKick color='white' size={50} /></a></li>
             </ul>
             <div className='trademark'>
                 <p>©June 2022, Niles is an Amazon Clone created by Billy Phan</p>
